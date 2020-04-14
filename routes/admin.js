9a165c81f65db4436b9a6d6adde86cf353a8b8e4
Hussain-Safwan <safwan.du16@gmail.com>
2020-04-14 21:36:38 +0600

    Just another heroku host

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../controllers/admin');
 
+router.get('/getUser', (req, res) => {
+    if (req.user) {
+        res.send(req.user)
+    } else {
+        res.send(null)
+    }
+})
+
 router.get('/login', controller.get_login);
 router.get('/forgot_password', controller.get_forgot_password);
 router.post('/forgot_password', controller.post_forgot_password);
