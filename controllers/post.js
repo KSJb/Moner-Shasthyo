477172545d0e069883d2369c5e2f274c78c02b1a
Hussain-Safwan <safwan.du16@gmail.com>
2020-07-25 18:31:00 +0600

    Just another heroku host

--- controllers/post.js
+++ controllers/post.js
@@ -41,8 +41,7 @@ const getTime = () => {
     }
     const offset = d.getTimezoneOffset()/60
     console.log(offset)
-    d.setHours(d.getHours() - offset )
-    let h = ((d.getHours() + 24) % 12 || 12).toString()
+    let h = ((d.getHours() - offset + 24) % 12 || 12).toString()
     let m = d.getMinutes().toString()
     if (m.length < 2) {
         m = '0' + m
